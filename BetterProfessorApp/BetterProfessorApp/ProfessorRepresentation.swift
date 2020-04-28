199de5ddd5b645c741e452f29dc0baf7b46f5c21
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-04-28 08:36:38 -0700

    Added in Cody's work from Monday

---
+++ BetterProfessorApp/BetterProfessorApp/ProfessorRepresentation.swift
@@ -0,0 +1,28 @@
+//
+//  ProfessorRepresentation.swift
+//  BetterProfessorApp
+//
+//  Created by Lambda_School_Loaner_268 on 4/28/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct ProfessorRepresentation: Codable {
+
+    //MARK: - Enums and Type Aliases -
+    enum CodingKeys: String, CodingKey {
+        case id, email, password, students
+        case firstName = "first_name"
+        case lastName = "last_name"
+    }
+
+    //MARK: - Properties -
+    let id: Int64
+    let email: String
+    let password: String
+    let firstName: String
+    let lastName: String
+    var students: [StudentRepresentation]
+}
