05fb8c2a727d66597e4a7de2a906051529a2cc39
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-04-27 23:44:31 -0700

    Log In/Sign Up Completed
    Setting up Tab Bar Controller for app.

---
+++ BetterProfessorApp/BetterProfessorApp/ViewControllers/LogInSignUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LogInSignUpViewController.swift
+//  BetterProfessorApp
+//
+//  Created by Lambda_School_Loaner_268 on 4/27/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class LogInSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var logoIV: UIImageView!
+    
+    @IBAction func signUpButton(_ sender: Any) {
+    }
+    @IBAction func logInButton(_ sender: Any) {
+    }
+    
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
