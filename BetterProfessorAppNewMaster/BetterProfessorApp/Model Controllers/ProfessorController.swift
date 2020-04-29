25d8033a71b8f08f8b0c2450cd5c7fe9991e1cde
Cody Morley <codymorleyinc@gmail.com>
2020-04-29 10:32:38 -0700

    safety commit. not much work here.

--- BetterProfessorAppNewMaster/BetterProfessorApp/Model Controllers/ProfessorController.swift
+++ BetterProfessorAppNewMaster/BetterProfessorApp/Model Controllers/ProfessorController.swift
@@ -8,25 +8,39 @@
 
 import Foundation
 import CoreData
-//MARK: - Core Data Functions -
-// Use these functions in the app to handle background logic on the Professor Model object
 
-func createProfessor() {
+class ProfessorController {
+    //MARK: - Properties -
+    // Fetched Results Controller - This will handle core data fetches for Professor model objects.
     
-}
-
-func fetchProfessor() {
     
-}
-
-func updateProfessor() {
     
-}
-
-func saveProfessor() {
     
-}
-
-func deleteProfessor() {
+    //MARK: - Core Data Functions -
+    // Use these functions in the app to handle background logic on the Professor Model object
     
+    func createProfessor() {
+        
+    }
+    
+    func fetchProfessor() {
+        
+    }
+    
+    func updateProfessor(professor: Professor, representation: ProfessorRepresentation) {
+        ///call this function to change user info or update students from a new server-side representation.
+        
+        professor.email = representation.email
+        professor.password = representation.password
+        professor.students = NSSet(array: representation.students)
+        
+    }
+    
+    func saveProfessor() {
+        
+    }
+    
+    func deleteProfessor() {
+        
+    }
 }
