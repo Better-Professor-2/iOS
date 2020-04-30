762ebc526d3da64c923f05c80d43930f66e9942b
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-04-30 01:25:19 -0700

    Storyboard and CocoaTouch Changes {
    	Removed {
    
    		MyProjectsViewController.swift
    		MyRemindersViewController.swift
    		MyStudentsViewController.swift
    
    	}
    
    	Added {
    
    		RemindersTableViewController.swift + Replaced instance of MyRemindersViewController in storyboard.
    		ProjectsTableViewController.swift + Replaced instance of MyProjectsViewController in storyboard.
    		StudentsTableViewController.swift + Replaced instance of MyStudentsViewController in storyboard.
    
    		Grouped CocoaTouch files into folders.
    	}
    }

---
+++ BetterProfessorAppNewMaster/BetterProfessorApp/ViewControllers/Main Tabs/Students/Projects TableView/ProjectsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectsTableViewCell.swift
+//  BetterProfessorApp
+//
+//  Created by Lambda_School_Loaner_268 on 4/30/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
