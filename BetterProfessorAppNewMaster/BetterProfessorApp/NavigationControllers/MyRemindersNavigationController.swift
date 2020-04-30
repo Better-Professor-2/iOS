6c5d36cf7b4a7375111dfe2194c3580ba2beee0c
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-04-29 18:43:47 -0700

    Added {
    	Navigation Controllers  {
    
    		MyRemindersNavigationController.swift
    		MyStudentsNavigationController.swift
    		MyProjectsNavigationController.swift
    		LogInSignUpNavigationController.swift
    		MainAppTabBarController.swift
    
    	}
    
    	View Controllers + CocoaTouch Files {
    
    		LogInSignUpViewController.swift
    		MyRemindersViewController.swift
    		AddReminderViewController.swift
    		ReminderDetailViewController.swift
    		MyStudentsViewController.swift
    		MyProjectsViewController.swift
    
    	}
    }

---
+++ BetterProfessorAppNewMaster/BetterProfessorApp/NavigationControllers/MyRemindersNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  MyRemindersNavigationController.swift
+//  BetterProfessorApp
+//
+//  Created by Lambda_School_Loaner_268 on 4/29/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class MyRemindersNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
