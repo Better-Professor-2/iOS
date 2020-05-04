e8e08859bb42b3e9fb2e03429bbfc612fb0b8ad7
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-05-04 09:16:52 -0700

    Unit Tests Completed.

--- BetterProfessorApp/BetterProfessorAppTests/BetterProfessorAppTests.swift
+++ BetterProfessorApp/BetterProfessorAppTests/BetterProfessorAppTests.swift
@@ -72,13 +72,19 @@ class BetterProfessorAppTests: XCTestCase {
     }
     
     func testMakeDeadline() {
-        let prof = Professor(id: 44,
-                                    email: "emailgmail@gmail.com", password: "aA12345!",
-                                    firstName: "John",
-                                    lastName: "Rambo",
-                                    students: [])
-        let student = studentCon.createStudent(for: prof, firstName: "Kenny", lastName: "Dykstra", email: "spiritsquad@gmail.com", phoneNumber: "8008675309")
-        XCTAssertNotNil(student)
+        let prof = Professor(id: 1111, email: "hello@gmail.com", password: "aA12345!",
+                             firstName: "Hector", lastName: "Salamanca",
+                             
+                             students: [], context: CoreDataStack.shared.mainContext)
+        let student = Student(id: 111,
+                                     firstName: "Henry",
+                                     lastName: "Cavill",
+                                     email: "thewitcher@gmail.com",
+                                     phoneNumber: "9008880000",
+                                     professor: prof, deadlines: [],
+                                     context: CoreDataStack.shared.mainContext)
+               let deadline = deadCon.createDeadline(for: student, name: "I hate deadlines!", dueDate: Date(), notes: "Notes!")
+        XCTAssertNotNil(deadline)
         
     }
     
@@ -95,6 +101,65 @@ class BetterProfessorAppTests: XCTestCase {
         XCTAssertNotNil(profController.fetchProfessor(context: CoreDataStack.shared.mainContext, id: 100))
     }
     
+    func testUpdateProf() {
+        let prof = Professor(id: 100, email: "lalilulelo", password: "aA12345!", firstName: "Richard", lastName: "Ames", students: [], context: CoreDataStack.shared.mainContext)
+        let profID = prof.id
+        var profRep = prof.professorRepresentation
+        profRep?.id = 44
+        profController.updateProfessor(professor: prof, representation: profRep!)
+        XCTAssert(prof.id == 44)
+    }
+
+    func testUpdateDeadline() {
+        let prof = Professor(id: 1111, email: "hello@gmail.com", password: "aA12345!", firstName: "Hector", lastName: "Salamanca", students: [], context: CoreDataStack.shared.mainContext)
+        let student = Student(id: 111,
+                              firstName: "Henry",
+                              lastName: "Cavill",
+                              email: "thewitcher@gmail.com",
+                              phoneNumber: "9008880000",
+                              professor: prof,
+                              deadlines: [],
+                              context: CoreDataStack.shared.mainContext)
+            deadCon.createDeadline(for: student,
+                                              name: "I hate deadlines!",
+                                              dueDate: Date(),
+                                              notes: "Notes!")
+        XCTAssertNotNil(student.deadlines)
+        var deadlines = student.deadlines
+        let deadline: Deadline = Deadline(id: 44, name: "I hate deadlines", dueDate: Date(), notes: "Hellloooo!", studentID: 111, student: student)
+        var deadlineRep: DeadlineRepresentation = DeadlineRepresentation(id: 44,
+                                                                         name: "redo",
+                                                                         dueDate: Date(), notes: "Now I have more notes",
+                                                                         studentID: 55,
+                                                                         notifications: [])
+        deadCon.updateDeadline(deadline: deadline,
+                               representation: deadlineRep)
+        let newDeadLine = deadCon.fetchDeadline(context: CoreDataStack.shared.mainContext, id: 44)
+        XCTAssertNotNil(newDeadLine)
+    }
     
+    func testDelete() {
+        let context = CoreDataStack.shared.mainContext
+        let deadCon = DeadlineController()
+        let prof = Professor(id: 1111, email: "hello@gmail.com", password: "aA12345!", firstName: "Hector", lastName: "Salamanca", students: [], context: CoreDataStack.shared.mainContext)
+        let student = Student(id: 111,
+                              firstName: "Henry",
+                              lastName: "Cavill",
+                              email: "thewitcher@gmail.com",
+                              phoneNumber: "9008880000",
+                              professor: prof,
+                              deadlines: [],
+                              context: CoreDataStack.shared.mainContext)
+        
+        let deadToDelete = Deadline(id: 44,
+                                    name: "I hate deadlines!",
+                                    dueDate: Date(),
+                                    notes: "NOOOOOOOOTes",
+                                    studentID: 111, student: student,
+                                    notifications: [],
+                                    context: context)
+        deadCon.deleteDeadline(deadline: deadToDelete)
+        XCTAssertNil(deadCon.fetchDeadline(context: context, id: 44))
+    }
     
 }
